9efed9161107a41751694f58e1a01e8596d0f56f
Jonathan García <megabyte9703@gmail.com>
2021-08-02 01:24:46 -0600

    Add cookie parser

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const user = require("./API/routes/user");
 const config = require("./config");
 const session = require("express-session");
 const passport = require("passport");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(
   session({
